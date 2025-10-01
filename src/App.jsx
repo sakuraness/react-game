6e1d21161710929d2d79398ba12ede659f5c355d
Allen Chiu <all982633@gmail.com>
2025-10-01 17:41:21 +0800

    Initial deploy path

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,8 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
-// import Game from './components/Game'; // 之後的頁面
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        {/* <Route path="/game" element={<Game />} /> */}
-      </Routes>
-    </Router>
-  );
+  return <Home />;
 }
 
 export default App;
